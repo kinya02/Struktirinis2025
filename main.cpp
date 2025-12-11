bcd1ab975595732dab69de446a0035feeb9a74b5
kinya02 <wysocka.kinga1@gmail.com>
2025-12-11 09:53:25 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,108 +1,118 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <string>
-
 using namespace std;
 
-void skaiciuotiBilietus() {
-    ifstream inFile("bilietai.txt");
-    ofstream outFile("rezultatai_bilietai.txt");
-
-    if (!inFile) {
-        cout << "Nepavyko atidaryti failo 'bilietai.txt'." << endl;
-        return;
-    }
-
-    string tipas;
-    double kaina;
-    int kiekis;
-    double bendraSuma = 0;
-    int bendrasKiekis = 0;
-
-    cout << fixed << setprecision(2);
-    outFile << fixed << setprecision(2);
-
-    cout << "Bilietu pardavimu duomenys:" << endl;
-    outFile << "Bilietu pardavimu duomenys:" << endl;
-
-    while (inFile >> tipas >> kaina >> kiekis) {
-        double suma = kaina * kiekis;
-        bendraSuma += suma;
-        bendrasKiekis += kiekis;
-
-        cout << tipas << " bilietai: "
-             << kiekis << " vnt, "
-             << "kaina: " << kaina << " EUR, "
-             << "suma: " << suma << " EUR" << endl;
-
-        outFile << tipas << " bilietai: "
-                << kiekis << " vnt, "
-                << "kaina: " << kaina << " EUR, "
-                << "suma: " << suma << " EUR" << endl;
-    }
-
-    cout << "Bendras parduotu bilietu kiekis: " << bendrasKiekis << endl;
-    cout << "Bendra pardavimu suma: " << bendraSuma << " EUR" << endl;
-
-    outFile << "Bendras parduotu bilietu kiekis: " << bendrasKiekis << endl;
-    outFile << "Bendra pardavimu suma: " << bendraSuma << " EUR" << endl;
-
-    inFile.close();
-    outFile.close();
-}
-
-void atnaujintiAtlyginimus() {
-    ifstream inFile("salary.txt");
-    ofstream outFile("newSalary.txt");
-
-    if (!inFile) {
-        cout << "Nepavyko atidaryti failo 'salary.txt'." << endl;
-        return;
-    }
-
-    string pavarde, vardas;
-    double atlyginimas, procentas;
-
-    cout << fixed << setprecision(2);
-    outFile << fixed << setprecision(2);
-
-    cout << "Atnaujinti atlyginimai:" << endl;
-
-    while (inFile >> pavarde >> vardas >> atlyginimas >> procentas) {
-        double naujas = atlyginimas + (atlyginimas * procentas / 100);
-        cout << pavarde << " " << vardas << " " << naujas << endl;
-        outFile << pavarde << " " << vardas << " " << naujas << endl;
-    }
-
-    inFile.close();
-    outFile.close();
-}
+struct menuItemType {
+    string name;
+    double price;
+};
 
 int main() {
-    int pasirinkimas;
 
-    do {
-        cout << " 1 - Skaiciuoti bilietu pardavimus" << endl;
-        cout << " 2 - Atnaujinti darbuotoju atlyginimus" << endl;
-        cout << " 0 - Baigti programa" << endl;
-        cout << "Pasirinkite veiksma: ";
-        cin >> pasirinkimas;
+    menuItemType menu[20]; //sukuriame masyva is 20 elementu, kur kiekvienas elementas yra struktura su name ir price
+    int count = 0;
+    int qty[20] = {0};
 
-        switch (pasirinkimas) {
-            case 1:
-                skaiciuotiBilietus();
-                break;
-            case 2:
-                atnaujintiAtlyginimus();
-                break;
-            case 0:
-                cout << "Programa baigta." << endl;
-                break;
-            default:
-                cout << "Neteisingas pasirinkimas. Bandykite dar karta." << endl;
+    ifstream fin("menu.txt");
+
+    if (!fin.is_open()) { //jeigu failas neatidarytas
+        cout << "Nepavyko atidaryti menu failo\n";
+        return 0;
+    }
+
+
+    while (fin >> menu[count].name >> menu[count].price) { //fin - skaityk
+        count++;
+    }
+
+    fin.close();
+
+    // Parodome meniu
+    cout << "Sveiki atvyke i restorana Kinga!\n\n";
+    cout << "Pusryciu meniu:\n";
+
+    for (int i = 0; i < count; i++) {
+        cout << i + 1 << ". "
+             << menu[i].name << " - "
+             << fixed << setprecision(2)
+             << menu[i].price << " EUR\n";
+    }
+
+    // Pasirinkimai
+    while (true) {
+
+        int choice;
+        cout << "\nPasirinkite patiekalo numeri (0 - baigti): ";
+        cin >> choice;
+
+        if (!cin) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Neteisingas ivestis! Bandykite dar karta.\n";
+            continue;
         }
-    } while (pasirinkimas != 0);
+
+        if (choice == 0) {
+            break;
+        }
+
+        if (choice < 1 || choice > count) {
+            cout << "Tokio patiekalo nera!\n";
+            continue;
+        }
+
+        int porsijos;
+        cout << "Kiek porciju norite: ";
+        cin >> porsijos;
+
+        if (!cin || porsijos < 1) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Neteisingas porciju kiekis!\n";
+            continue;
+        }
+
+        qty[choice - 1] += porsijos;
+        cout << "Prideta!\n";
+    }
+
+    // Skaiciavimai
+    double suma = 0;
+
+    ofstream fout("receipt.txt");
+
+    cout << "\nJusu uzsakymas:\n\n";
+    fout << "Jusu uzsakymas:\n\n";
+
+    for (int i = 0; i < count; i++) {
+        if (qty[i] > 0) {
+
+            double tarpine = qty[i] * menu[i].price;
+            suma += tarpine;
+
+            cout << qty[i] << " x " << menu[i].name
+                 << " = " << fixed << setprecision(2)
+                 << tarpine << " EUR\n";
+
+            fout << qty[i] << " x " << menu[i].name
+                 << " = " << fixed << setprecision(2)
+                 << tarpine << " EUR\n";
+        }
+    }
+
+    double pvm = suma * 0.21;
+    double galutine = suma + pvm;
+
+    cout << "\nMokesciai (21%): " << fixed << setprecision(2) << pvm << " EUR\n";
+    cout << "Galutine suma: " << galutine << " EUR\n";
+
+    fout << "\nMokesciai (21%): " << pvm << " EUR\n";
+    fout << "Galutine suma: " << galutine << " EUR\n";
+
+    fout.close();
+
+    cout << "\nSaskaita irasyta i receipt.txt\n";
 
     return 0;
 }
